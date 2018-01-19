7f6809b456440da322c58b4655561425fcab75c8
BrainStone <yannick@tedworld.de>
2018-01-19 13:26:00 +0100

    Added end condition

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
         int seed;
         
         try {
-            seed = std::stoi(argv[0]);
+            seed = std::stoi(argv[1]);
         } catch(...) {
             // Ignore all errors
             seed = 0;
@@ -14,23 +14,33 @@ int main(int argc, char* argv[]) {
         randomizeWeights(s * s, W, seed);
     }
     
+    size_t oldScore;
+    size_t score;
+    size_t t = 0;
+    
 	hexDumpMemory(W, s*s);
 	std::cout << "Training:\t" << s << std::endl;
 	std::cout << "Count:\t\t" << sizeMobyDick - 1 << std::endl;
 
-	std::cout << "t = 0:\t\t" << calculateScore( s, w, S ) << std::endl;
+    score = calculateScore( s, w, S );
+    
+	std::cout << "t = 0:\t\t" << score << std::endl;
 
-	for ( size_t i = 0; i < 10; ++i ) {
+	do {
 		train( s, w, W, S );
 		saveWeights( std::to_string(s), s, W );
-		std::cout << "t = " << i + 1 << ":\t\t" << calculateScore( s, w, S ) << std::endl;
-	}
+        
+        oldScore = score;
+        score = calculateScore( s, w, S );
+        
+		std::cout << "t = " << ++t << ":\t\t" << score << std::endl;
+	} while ( oldScore != score );
 }
 
 void randomizeWeights( const size_t weightsSize, uint8_t* weights, const uint8_t seed ) {
     srand(seed);
     
     for(size_t i = 0; i < weightsSize; ++i) {
-        weights[i] = (uint8_t) rand();
+        weights[i] = ((uint8_t) rand()) & 0x7F;
     }
 }
